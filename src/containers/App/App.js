49f92dc8e16e5db7668d71c6b024e6ca6f7da99a
robertschaap <robert.schaap@mac.com>
2018-09-25 22:45:34 +0200

    App: replaced placeholder with Header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Form from "../../components/Form";
+import Header from "../../components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        ReduxForm
+        <Header>Redux Form</Header>
         <Form />
       </div>
     );
